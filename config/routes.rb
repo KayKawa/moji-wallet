fac9c1de95c04b48b13325e23c667b20bf808696
KayKawa <goodenoughliving1221@gmail.com>
2022-08-30 09:40:13 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
              }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "homes#index"
-  resources :wallets, only: [:index]
-  resources :transaction
+  resources :wallets, only: [:index] do
+    resources :transaction
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
