d35a38b8454615be8aa2bd838959206f18c5b653
KayKawa <goodenoughliving1221@gmail.com>
2022-08-30 11:24:34 +0900

    walletのviewの雛形

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,5 +1,8 @@
 class WalletsController < ApplicationController
   def show
     @wallet = Wallet.find_by(url: params[:url])
+    wallet_user = @wallet.user_id
+    user = User.find_by(id: wallet_user)
+    @profile = user.profile
   end
 end
