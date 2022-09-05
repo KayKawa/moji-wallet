6807811cdfcd76e6a1b3e66054e240e0a6d5eb94
KayKawa <goodenoughliving1221@gmail.com>
2022-09-05 14:08:20 +0900

    transaction - message アソシエーション

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,5 @@
 class Transaction < ApplicationRecord
   belongs_to :pay_u, class_name: "User", foreign_key: "pay_u_id"
   belongs_to :beneficiary_u, class_name: "User", foreign_key: "beneficiary_u_id"
+  has_one :message
 end
