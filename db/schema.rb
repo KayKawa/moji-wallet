7a252597e5fe36ddf7d7727bd1714e0c5b1be7c1
KayKawa <goodenoughliving1221@gmail.com>
2022-09-06 15:50:56 +0900

    usersテーブルにstripe関連のカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_05_062532) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_065000) do
   create_table "messages", charset: "utf8", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "trade_id"
@@ -50,6 +50,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_062532) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
+    t.string "provider"
+    t.string "access_code"
+    t.string "publishable_key"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
