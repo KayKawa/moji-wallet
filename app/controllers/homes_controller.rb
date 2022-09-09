a9682e0a36091925ee89c905bcd3805f0e90d5ec
KayKawa <goodenoughliving1221@gmail.com>
2022-09-09 09:54:17 +0900

    kickoff tailwindのviewを試す

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,8 @@
 class HomesController < ApplicationController
   def index
+    if user_signed_in?
+      user = User.find(current_user.id)
+      @wallet = Wallet.find_by(user_id: user)
+    end
   end
 end
