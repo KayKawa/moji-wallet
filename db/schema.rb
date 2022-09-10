bc5ee16c48fc075f15e445be96da82bbc64e2b06
KayKawa <goodenoughliving1221@gmail.com>
2022-09-10 14:27:04 +0900

    Userモデルcustomer_idカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_065000) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_09_060201) do
   create_table "messages", charset: "utf8", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "trade_id"
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_065000) do
     t.string "provider"
     t.string "access_code"
     t.string "publishable_key"
+    t.string "customer_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
