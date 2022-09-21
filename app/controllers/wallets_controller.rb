8dd341bb91b531a47397cb3d7f637f2db8287e50
KayKawa <goodenoughliving1221@gmail.com>
2022-09-21 12:38:35 +0900

    初回stripe連携のリダイレクト＃後に使用する

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -4,5 +4,8 @@ class WalletsController < ApplicationController
     wallet_user = @wallet.user_id
     user = User.find_by(id: wallet_user)
     @profile = user.profile
+
+    # redirect_to "https://connect.stripe.com/oauth/authorize?response_type=code&client_id=#{ENV["STRIPE_CLIENT_ID"]}&stripe_user[email]=#{current_user.email}&stripe_user[url]=http://localhost:3000/#{@wallet.url}&scope=read_write",
+    #             allow_other_host: true
   end
 end
